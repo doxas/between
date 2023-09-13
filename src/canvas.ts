f8296964505f393c6ae5051a8cbe3fa9f4fd3a8e
doxas <eizanco@gmail.com>
2023-09-13 17:45:05 +0900

    [wip] implement

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,19 +1,57 @@
+import { initializeBuffer } from "@tweakpane/core";
+import { WebGLUtility } from './webgl';
 
 export class Renderer {
   private parent: HTMLElement;
   private canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D;
   private image: HTMLImageElement;
+  private glCanvas: HTMLCanvasElement;
+  private gl: WebGLRenderingContext;
+  private isRendering: boolean;
+  private texture: WebGLTexture;
 
   constructor(parent: HTMLElement) {
     this.parent = parent;
     this.canvas = document.createElement('canvas');
     this.context = this.canvas.getContext('2d');
+    this.glCanvas = document.createElement('canvas');
+    this.gl = this.canvas.getContext('webgl');
+
+    this.canvas.style.width = '100%';
+    this.canvas.style.height = '100%';
+    this.canvas.style.position = 'absolute';
+    this.canvas.style.top = '0px';
+    this.canvas.style.left = '0px';
+    this.glCanvas.style.width = '100%';
+    this.glCanvas.style.height = '100%';
+    this.glCanvas.style.position = 'absolute';
+    this.glCanvas.style.top = '0px';
+    this.glCanvas.style.left = '0px';
+    this.parent.appendChild(this.glCanvas);
     this.parent.appendChild(this.canvas);
 
+    this.init();
     this.eventSetting();
   }
 
+  init(): void {
+    this.isRendering = false;
+    if (this.gl == null) {
+      throw new Error('webgl not support.');
+    }
+    this.texture = WebGLUtility.createTexture(this.gl, this.image);
+  }
+  update(): void {
+    if (this.gl == null || this.image == null) {return;}
+
+    if (this.isRendering !== true) {
+      this.isRendering = true;
+      this.render();
+    }
+  }
+  render(): void {
+  }
   eventSetting(): void {
     const body = document.body;
     body.addEventListener('dragover', (evt) => {
@@ -30,13 +68,11 @@ export class Renderer {
         const url = reader.result as string;
         this.image = new Image();
         this.image.addEventListener('load', () => {
-          this.render();
+          this.update();
         }, false);
         this.image.src = url;
       }, false);
       reader.readAsDataURL(files[0]);
     }, false);
-  }
-  render(): void {
   }
 }
