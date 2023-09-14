763f94e7bdb1b114913b06a51f29b5b002c44d87
doxas <eizanco@gmail.com>
2023-09-14 16:48:58 +0900

    [wip] temp

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,5 +1,5 @@
 import { initializeBuffer } from "@tweakpane/core";
-import { WebGLUtility } from './webgl';
+import { ShaderProgram, WebGLUtility } from './webgl';
 
 export class Renderer {
   private parent: HTMLElement;
@@ -10,13 +10,27 @@ export class Renderer {
   private gl: WebGLRenderingContext;
   private isRendering: boolean;
   private texture: WebGLTexture;
+  private vertexShaderSource: string;
+  private fragmentShaderSource: string;
+  private shaderProgram: ShaderProgram;
+  private position: number[];
+  private texCoord: number[];
+  private offset: number[];
+  private indices: number[];
+  private vbo: WebGLBuffer[];
+  private ibo: WebGLBuffer;
+
+  private uCrevice: number[];
+  private uMouse: number[];
+  private uCanvasAspect: number;
+  private uResourceAspect: number;
 
   constructor(parent: HTMLElement) {
     this.parent = parent;
     this.canvas = document.createElement('canvas');
     this.context = this.canvas.getContext('2d');
     this.glCanvas = document.createElement('canvas');
-    this.gl = this.canvas.getContext('webgl');
+    this.gl = this.glCanvas.getContext('webgl');
 
     this.canvas.style.width = '100%';
     this.canvas.style.height = '100%';
@@ -31,6 +45,10 @@ export class Renderer {
     this.parent.appendChild(this.glCanvas);
     this.parent.appendChild(this.canvas);
 
+    this.render = this.render.bind(this);
+    this.resize = this.resize.bind(this);
+
+    this.resize();
     this.init();
     this.eventSetting();
   }
@@ -40,10 +58,141 @@ export class Renderer {
     if (this.gl == null) {
       throw new Error('webgl not support.');
     }
-    this.texture = WebGLUtility.createTexture(this.gl, this.image);
+    const gl = this.gl;
+    this.position = [];
+    this.texCoord = [];
+    this.offset = [];
+    this.indices = [];
+    const ROW = 2;
+    const COLUMN = 2;
+    const COUNT_X = (COLUMN - 1) * 2 + 2;
+    const SIZE = 2;
+    const start = -SIZE / 2;
+    const wx = SIZE / COLUMN;
+    const wy = SIZE / ROW;
+    let iI = 0;
+    for (let i = 0; i <= ROW; ++i) {
+      const y = start + i * wy;
+      const v = i / ROW;
+      const dr = (i === 0 || i === ROW) ? 1 : 2;
+      for (let I = 0; I < dr; ++I) {
+        let iJ = 0;
+        for (let j = 0; j <= COLUMN; ++j) {
+          const x = start + j * wx;
+          const u = j / COLUMN;
+          const dc = (j === 0 || j === COLUMN) ? 1 : 2;
+          for (let J = 0; J < dc; ++J) {
+            const cy = i === 0 ? 1.0 : -1.0;
+            const cx = j === 0 ? -1.0 : 1.0;
+            this.position.push(x, -y);
+            this.texCoord.push(u, v);
+            this.offset.push(cx, cy);
+
+            if (i > 0 && j > 0) {
+              const c = iI * COUNT_X + (iJ - 1);
+              const p = c - COUNT_X;
+              this.indices.push(p, c, p + 1, p + 1, c, c + 1);
+            }
+            ++iJ;
+          }
+        }
+        ++iI;
+      }
+    }
+    this.vbo = [
+      WebGLUtility.createVbo(gl, this.position),
+      WebGLUtility.createVbo(gl, this.texCoord),
+      WebGLUtility.createVbo(gl, this.offset),
+    ];
+    this.ibo = WebGLUtility.createIbo(gl, this.indices);
+
+    this.vertexShaderSource = `
+      attribute vec2 position;
+      attribute vec2 texCoord;
+      attribute vec2 offset;
+      uniform vec2 crevice;
+      uniform vec2 mouse;
+      uniform float canvasAspect;
+      uniform float resourceAspect;
+      varying vec4 vColor;
+      varying vec2 vTexCoord;
+      void main() {
+        vTexCoord = texCoord;
+
+        vec2 o = offset * crevice;
+        vec2 p = position + o;
+        p = vec2(p.x, p.y / resourceAspect);
+        if (canvasAspect > 1.0) {
+          p.x /= canvasAspect;
+        } else {
+          p.y *= canvasAspect;
+        }
+
+        gl_Position = vec4(p, 0.0, 1.0);
+        gl_Position = vec4(position, 0.0, 1.0);
+
+        vColor = vec4(position * 0.5 + 0.5, 0.0, 1.0);
+      }
+    `;
+    this.fragmentShaderSource = `
+      precision highp float;
+      uniform sampler2D inputTexture;
+      varying vec2 vTexCoord;
+      varying vec4 vColor;
+      void main() {
+        vec4 samplerColor = texture2D(inputTexture, vTexCoord);
+        gl_FragColor = samplerColor * vColor;
+        gl_FragColor = vColor;
+      }
+    `;
+
+    this.shaderProgram = new ShaderProgram(gl, {
+      vertexShaderSource: this.vertexShaderSource,
+      fragmentShaderSource: this.fragmentShaderSource,
+      attribute: [
+        'position',
+        'texCoord',
+        'offset',
+      ],
+      stride: [
+        2,
+        2,
+        2,
+      ],
+      uniform: [
+        'crevice',
+        'mouse',
+        'canvasAspect',
+        'resourceAspect',
+        'inputTexture',
+      ],
+      type: [
+        'uniform2fv',
+        'uniform2fv',
+        'uniform1f',
+        'uniform1f',
+        'uniform1i',
+      ],
+    });
+    this.shaderProgram.use();
+    this.shaderProgram.setAttribute(this.vbo, this.ibo);
+
+    gl.clearColor(0.5, 0.5, 0.5, 1.0);
+
+    this.uCrevice = [0, 0];
+    this.uMouse = [0.0, 0.0];
   }
   update(): void {
     if (this.gl == null || this.image == null) {return;}
+    const gl = this.gl;
+
+    if (this.texture != null) {
+      gl.bindTexture(gl.TEXTURE_2D, null);
+      gl.deleteTexture(this.texture);
+      this.texture = null;
+    }
+    this.texture = WebGLUtility.createTexture(gl, this.image);
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
 
     if (this.isRendering !== true) {
       this.isRendering = true;
@@ -51,8 +200,23 @@ export class Renderer {
     }
   }
   render(): void {
+    const gl = this.gl;
+    requestAnimationFrame(this.render);
+    gl.viewport(0, 0, this.glCanvas.width, this.glCanvas.height);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    this.shaderProgram.setUniform([
+      this.uCrevice,
+      this.uMouse,
+      this.uCanvasAspect,
+      this.uResourceAspect,
+      0,
+    ]);
+
+    gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_BYTE, 0);
   }
   eventSetting(): void {
+    window.addEventListener('resize', this.resize, false);
     const body = document.body;
     body.addEventListener('dragover', (evt) => {
       evt.preventDefault();
@@ -68,11 +232,26 @@ export class Renderer {
         const url = reader.result as string;
         this.image = new Image();
         this.image.addEventListener('load', () => {
+          this.uResourceAspect = this.image.naturalWidth / this.image.naturalHeight;
           this.update();
         }, false);
         this.image.src = url;
       }, false);
       reader.readAsDataURL(files[0]);
     }, false);
+
+    this.canvas.addEventListener('pointermove', (pointerEvent) => {
+      const x = pointerEvent.pageX / window.innerWidth * 2.0 - 1.0;
+      const y = pointerEvent.pageY / window.innerHeight * 2.0 - 1.0;
+      this.uMouse[0] = x;
+      this.uMouse[1] = -y;
+    }, false);
+  }
+  resize() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.glCanvas.width = window.innerWidth;
+    this.glCanvas.height = window.innerHeight;
+    this.uCanvasAspect = window.innerWidth / window.innerHeight;
   }
 }
